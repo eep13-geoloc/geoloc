4ec760fa20853319060128ef54ee1f498e48ed19
Jyoti Badhe <jyoti.badhe@accenture.com>
2018-10-03 22:11:29 +0530

    Notification code added.

--- app/src/main/java/com/hsbc/eep/geoloc/service/LocationJobService.java
+++ app/src/main/java/com/hsbc/eep/geoloc/service/LocationJobService.java
@@ -1,12 +1,15 @@
 package com.hsbc.eep.geoloc.service;
 
 import android.Manifest;
+import android.app.NotificationManager;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.location.places.PlaceDetectionClient;
@@ -15,11 +18,14 @@ import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.hsbc.eep.geoloc.geoloc.R;
 import com.hsbc.eep.geoloc.model.Place;
 import com.hsbc.eep.geoloc.util.Util;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LocationJobService extends JobService {
     private static final String TAG = "SyncService";
@@ -70,9 +76,44 @@ public class LocationJobService extends JobService {
     private void sendNotificationIntents(List<Place> places) {
         //TODO send intent to processing service or do filtering here and send notifications
         Log.i(TAG, "Got " + places.size());
+
+        Map<String, String> offersMap = new HashMap<String, String>();
+        offersMap.put("Nike","10");
+        offersMap.put("Adidas","10");
+        offersMap.put("TGIF","15");
+        offersMap.put("Apple Store","5");
+        offersMap.put("Samsung","8");
+
+        String strNot1="There is flat ";
+        String strNot2="% off on ";
+        String strNot3=" Store Near You. Ejoy!!!";
+        String finalNotification="";
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<places.size();i++){
+            String offer= offersMap.get(places.get(i).getName());
+
+            sb = sb.append(strNot1).append(offer).append(strNot2).append(places.get(i).getName()).append(strNot3);
+            sb.append(System.getProperty("line.separator"));
+            //finalNotification = finalNotification.concat(strNot1+offer+strNot2+places.get(i).getName()+strNot3+"\n");
+        }
+
+        finalNotification=sb.toString();
+        sendNotification(finalNotification);
+        finalNotification="";
+
     }
 
+    private void sendNotification(String strNot){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle("HSBC Offers")
+                .setContentText(strNot)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC);//to show content in lock screen
 
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(001, builder.build());
+    }
 
     @Override
     public boolean onStopJob(JobParameters params) {
