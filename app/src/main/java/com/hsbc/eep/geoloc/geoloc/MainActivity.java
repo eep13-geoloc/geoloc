3bee79a61eb9c539a36e22a012cacd8ba27512c1
Rafal Gielczowski <rafalg7@gmail.com>
2018-10-03 12:52:37 +0200

    Add test code to fetch current place

--- app/src/main/java/com/hsbc/eep/geoloc/geoloc/MainActivity.java
+++ app/src/main/java/com/hsbc/eep/geoloc/geoloc/MainActivity.java
@@ -1,13 +1,78 @@
 package com.hsbc.eep.geoloc.geoloc;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int gpsPermissionsReturnCode = 7;
+
+    private void getCurrentPlace() {
+        PlaceDetectionClient mPlaceDetectionClient =
+                Places.getPlaceDetectionClient(this);
+
+        Task<PlaceLikelihoodBufferResponse> placeResult =
+                mPlaceDetectionClient.getCurrentPlace(null);
+
+        placeResult.addOnCompleteListener(new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
+                if(likelyPlaces != null) {
+                    for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                        Log.i("LOC", String.format("Place '%s' has likelihood: %g",
+                                placeLikelihood.getPlace().getName(),
+                                placeLikelihood.getLikelihood()));
+                    }
+                    likelyPlaces.release();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case gpsPermissionsReturnCode: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getCurrentPlace();
+                } else {
+                    Log.i("LOC", "Failed to grant permissions!");
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("LOC", "permissions for geo location are not granted");
+            final String missingPerimissions[] = { Manifest.permission.ACCESS_FINE_LOCATION };
+            ActivityCompat.requestPermissions(this, missingPerimissions, gpsPermissionsReturnCode);
+        } else {
+            getCurrentPlace();
+        }
     }
 }
